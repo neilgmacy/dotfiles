fe32bc82db001481d13c140b1d1b7bf624f12330
Neil Macy <neil@neilmacy.co.uk>
2022-02-06 20:23:35 +0000

    update install script
    installs homebrew and oh-my-zsh now. To be tested!

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,8 +1,24 @@
+# install Xcode command line tools
+
+xcode-select --install
+
+# install Homebrew if not already present,
+# and install from Brewfile
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew bundle
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# simlink dotfiles
 ln -s ~/Dev/dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/Dev/dotfiles/git/.gitignore_global ~/.gitignore_global
 
 ln -s ~/Dev/dotfiles/vim/.vimrc ~/.vimrc
 
-./osx/set_defaults.sh
 ln -s ~/Dev/dotfiles/zsh/.zshrc ~/.zshrc
 
+# set preferred macOS default settings
+./macOS/set_defaults.sh

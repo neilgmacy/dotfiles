2cc19784e6ac7bb5421bce282a3c88c9d35c7c38
Neil Macy <neil@neilmacy.co.uk>
2022-02-07 11:13:06 +0000

    pass path to dotfiles repo as arg to script

--- README.md
+++ README.md
@@ -6,4 +6,10 @@ Feel free to copy/reuse/improve what you like, and if there's something particul
 
 ## Setup
 
-To automatically apply all dotfiles, run the `install_dotfiles.sh` script. This will require some adjustments: it assumes your `dotfiles` repo is in `~/Dev/`.
+There's a setup script called `install_dotfiles.sh`. This script does everything needed to get my Mac set up how I like it:
+- Install Xcode command line tools
+- Install [Homebrew](https://brew.sh)
+- Install [oh-my-zsh](https://ohmyz.sh), a tool to extend the zsh shell
+- Install packages and applications using `Brewfile`
+- Simlink the dotfiles in this repo, so that the working version of the file is always in this repo
+- Set my preferred Mac settings from `./macOS/set_defaults.sh`
--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,24 +1,33 @@
-# install Xcode command line tools
+#!/bin/sh
 
+# check dotfiles path was supplied
+
+if [ $# -ne 1 ];then
+  echo "Please pass the path of the dotfiles directory, e.g. /Users/you/dotfiles"
+  exit 1
+fi
+
+DOTFILES_PATH=$1
+
+# install Xcode command line tools
 xcode-select --install
 
-# install Homebrew if not already present,
-# and install from Brewfile
-
+# install Homebrew if not already present
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-brew bundle
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Install Homebrew packages and other apps
+brew bundle
+
 # simlink dotfiles
-ln -s ~/Dev/dotfiles/git/.gitconfig ~/.gitconfig
-ln -s ~/Dev/dotfiles/git/.gitignore_global ~/.gitignore_global
+ln -s $DOTFILES_PATH/git/.gitconfig ~/.gitconfig
+ln -s $DOTFILES_PATH/git/.gitignore_global ~/.gitignore_global
 
-ln -s ~/Dev/dotfiles/vim/.vimrc ~/.vimrc
+ln -s $DOTFILES_PATH/vim/.vimrc ~/.vimrc
 
-ln -s ~/Dev/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s $DOTFILES_PATH/zsh/.zshrc ~/.zshrc
 
 # set preferred macOS default settings
-./macOS/set_defaults.sh
+$DOTFILES_PATH/macOS/set_defaults.sh
